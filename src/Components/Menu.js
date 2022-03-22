ab4ad6a1c963e576930579b958ea13e669ed880a
PedroLopes7K <pedrohenri3601@gmail.com>
2022-03-22 19:35:37 -0300

    CRUD React

---
+++ src/Components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Menu(props) {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
