245de4de6b5675b429df89ed48caba2426beda09
PedroLopes7K <pedrohenri3601@gmail.com>
2022-03-23 21:15:36 -0300

    .

---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div>
+      <h1>404!</h1>
+      <p>
+        Lamento. Essa página não existe ou foi removida <br />
+        <a href="#">Voltar para a tabela de livros</a>
+      </p>
+    </div>
+  )
+}
